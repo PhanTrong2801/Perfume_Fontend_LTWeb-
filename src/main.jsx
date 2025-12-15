1ac0598b5b84d455edcd363ed1dc11288c40f39f
TrongPhan <trongphan280104@gmail.com>
2025-12-15 20:21:29 +0700

    update index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
 // Import Bootstrap (Bắt buộc phải cài: npm install bootstrap)
 import 'bootstrap/dist/css/bootstrap.min.css';
